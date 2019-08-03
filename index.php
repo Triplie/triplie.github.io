fccc0a1b21375ce98f7ebc47633c7de5c200a2d6
Triplie <51705362+Triplie@users.noreply.github.com>
2019-08-03 20:27:03 +0300

    cx

--- index.php
+++ index.php
@@ -1,21 +1,13 @@
- 
-<?php
-	include_once "header.php";
-?>
-		<link rel='stylesheet' type='text/css' href='sait.css'>
+
+		
 		<div class = "home-gallery">
 			gallery
 		</div>
 
 		<div class = "home-news">
-			<a href="/news">Новости</a>
+			<a>Новости</a>
 		</div>
 
 		<div class = "home-articles">
 			articles
 		</div>
-
-
-<?php
-	include "footer.php";
-?>
