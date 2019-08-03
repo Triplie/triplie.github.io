d4c10148f8d0eaacf7809af42427d8cc8d1b49c4
Triplie <51705362+Triplie@users.noreply.github.com>
2019-08-03 20:24:28 +0300

    initial commit

--- footer.php
+++
@@ -1,7 +0,0 @@
-	<div id ="footer">
-		<footer >
-			FOOTER
-		</footer>
-	</div>
-</body>
-</html>
--- header.php
+++
@@ -1,9 +0,0 @@
-<html>
-	<head>
-		<title> My site </title>
-		<link rel="icon" href="step.jpg">
-		<link rel='stylesheet'  href='Sait.css'>
-	</head>
-	<div id ="header">
-		<header> HEADER </header>
-	</div>
